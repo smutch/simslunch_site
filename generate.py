6f427caddcc8f5d702cd06710f9dd82d707b2787
Simon Mutch <smutch@unimelb.edu.au>
2015-08-06 22:34:45 +1000

    Split postdocs and students

--- generate.py
+++ generate.py
@@ -1,55 +1,71 @@
-from jinja2 import Environment, FileSystemLoader
+import warnings
+import random
 import yaml
 import pandas as pd
-import random
-import warnings
 import toyplot
 import toyplot.html
+from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader('templates'))
-
+# read in the list of members and their presenting histories
 with open('members.yaml', 'r') as fd:
     members = yaml.load(fd)
 
+# convert to a pandas dataframe
 members = pd.DataFrame.from_dict(members).T
 members.available.fillna(True, inplace=True)
 members.index.name = 'name'
 
+# generate plots of the current standings and render them to html files
 for contribution in ["Papers", "Plots"]:
     canvas, axes, mark = toyplot.bars(members[contribution.lower()], width=60+80*members.shape[0], height=300)
     axes.x.ticks.locator = toyplot.locator.Explicit(labels=members.index)
     axes.label.text = contribution
     axes.y.label.text = "Times presented"
     axes.y.ticks.locator = toyplot.locator.Integer()
+    toyplot.html.render(canvas, fobj="templates/"+contribution.lower()+"_history.html")
 
-    with open("templates/"+contribution.lower()+"_history.html", "wb") as fd:
-        #  fd.write(bytes('{% extends "index.html" %}'+"\n"
-        #        +"{% block "+contribution.lower()+"_history %}\n", 'UTF-8'))
-        toyplot.html.render(canvas, fobj=fd)
-        #  fd.write(bytes("\n{% endblock %}\n", 'UTF-8'))
-
+# cut down the members to just those who are available this coming week and
+# split by type
 members = members.query('available')
+postdocs = members.query('type == "postdoc"')
+postdocs.name = "postdocs"
+postdocs.presenters = dict(
+    paper = "",
+    plot = ""
+)
+students = members.query('type == "student"')
+students.name = "students"
+students.presenters = postdocs.presenters.copy()
 
-n_presenters = 2
+# choose the paper presenters randomly from those who have presented the
+# minimum number of times.
+for contribution in ('papers', 'plots'):
+    for group in (students, postdocs):
+        mi = group[contribution].min()
+        pool = list(group.query(contribution + ' == @mi').index)
+        group.presenters[contribution[:-1]] = random.sample(pool, 1)[0]
 
-mi = members.papers.min()
-pool = list(members.query('papers == @mi').index)
-paper_presenters = random.sample(pool, n_presenters)
-
-mi = members.plots.min()
-pool = list(members.query('plots == @mi').index)
-plot_presenters = random.sample(pool, n_presenters)
-
+# if we have someone who is meant to be presenting both types of
+# contributions then keep randomizing the plot presenter choce until we get
+# a valid combination (if possible)
 max_tries = 100
 n_tries = 0
-while len(set(plot_presenters + paper_presenters)) != 4:
-    plot_presenters = random.sample(pool, n_presenters)
-    n_tries+=1
-    if n_tries == max_tries:
-        warnings.warn("Failed to find unique paper + plot presenters!")
-        break
+#  import pdb
+#  pdb.set_trace()
+for group in (students, postdocs):
+    while len(set(group.presenters.values())) < 2:
+        mi = group["plots"].min()
+        pool = list(group.query('plots == @mi').index)
+        group.presenters["plot"] = random.sample(pool, 1)[0]
+        n_tries+=1
+        if n_tries == max_tries:
+            warnings.warn("Failed to find unique paper + plot presenters for"
+                          " the %s!" % group.name)
+            break
 
+# render the page
+env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('index.html')
 with open('build/index.html', 'w') as fd:
-    fd.write(template.render(paper_presenters=paper_presenters,
-                             plot_presenters=plot_presenters))
+    fd.write(template.render(postdoc_presenters=postdocs.presenters,
+                             student_presenters=students.presenters))
