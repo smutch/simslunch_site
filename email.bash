3e7a9e7a2cb3dafcb1e4363e5054136c5a883c8e
Yuxiang Qin <yuxiang.l.qin@gmail.com>
2016-06-24 14:04:54 +1000

    increment 24/06/16

--- email.bash
+++ email.bash
@@ -1,52 +1,7 @@
-mail -s '(Next Week) speaker on the simulation lunch meeting' geil.p@unimelb.edu.au <<< 'Hi Paul G,
+mail -s '(2 Weeks Later) speaker on the simulation lunch meeting' lwolz@unimelb.edu.au <<< 'Hi Laura,
 
-Please allow me to remind you that you are the speaker (for paper) for the simulation lunch meeting next week (30/June/16). (http://smutch.github.io/simslunch_site/index.html)
-
-Cheers,
-Yuxiang'
-mail -s '(Next Week) speaker on the simulation lunch meeting' yuxiangq@student.unimelb.edu.au <<< 'Hi Yuxiang,
-
-Please allow me to remind you that you are the speaker (for paper) for the simulation lunch meeting next week (30/June/16). (http://smutch.github.io/simslunch_site/index.html)
-
-Cheers,
-Yuxiang'
-mail -s '(2 Weeks Later) speaker on the simulation lunch meeting' edoardo.tescari@unimelb.edu.au <<< 'Hi Edoardo,
-
-You are selected to be the speaker (for paper) for the simulation lunch meeting to be held 2 weeks later (07/July/16). (http://smutch.github.io/simslunch_site/index.html)
-Please let me know if you do not want to present a paper:)
-
-Cheers,
-Yuxiang'
-mail -s '(2 Weeks Later) speaker on the simulation lunch meeting' gpoole@unimelb.edu.au <<< 'Hi Greg,
-
-You are selected to be the speaker (for paper) for the simulation lunch meeting to be held 2 weeks later (07/July/16). (http://smutch.github.io/simslunch_site/index.html)
-Please let me know if you do not want to present a paper:)
-
-Cheers,
-Yuxiang'
-mail -s '(Next Week) speaker on the simulation lunch meeting' gpoole@unimelb.edu.au <<< 'Hi Greg,
-
-Please allow me to remind you that you are the speaker (for plot) for the simulation lunch meeting next week (30/June/16). (http://smutch.github.io/simslunch_site/index.html)
-
-Cheers,
-Yuxiang'
-mail -s '(Next Week) speaker on the simulation lunch meeting' lwolz@unimelb.edu.au <<< 'Hi Laura,
-
-Please allow me to remind you that you are the speaker (for plot) for the simulation lunch meeting next week (30/June/16). (http://smutch.github.io/simslunch_site/index.html)
-
-Cheers,
-Yuxiang'
-mail -s '(2 Weeks Later) speaker on the simulation lunch meeting' manodeep.sinha@unimelb.edu.au <<< 'Hi Manodeep,
-
-You are selected to be the speaker (for plot) for the simulation lunch meeting to be held 2 weeks later (07/July/16). (http://smutch.github.io/simslunch_site/index.html)
-Please let me know if you do not want to present a plot:)
-
-Cheers,
-Yuxiang'
-mail -s '(2 Weeks Later) speaker on the simulation lunch meeting' hansikk@unimelb.edu.au <<< 'Hi Hank,
-
-You are selected to be the speaker (for plot) for the simulation lunch meeting to be held 2 weeks later (07/July/16). (http://smutch.github.io/simslunch_site/index.html)
-Please let me know if you do not want to present a plot:)
+You are selected to be the speaker (for papers) for the simulation lunch meeting to be held 2 weeks later (07/July/16). (http://smutch.github.io/simslunch_site/index.html)
+Please let me know if you do not want to present a papers:)
 
 Cheers,
 Yuxiang'
