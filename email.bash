2477fe022e1a140cdb0f2bcfecd2f41b82a6d0ea
Yuxiang Qin <yuxiang.l.qin@gmail.com>
2016-06-30 17:19:53 +1000

    increment 30/06/16

--- email.bash
+++ email.bash
@@ -1,7 +1,7 @@
-mail -s '(2 Weeks Later) speaker on the simulation lunch meeting' lwolz@unimelb.edu.au <<< 'Hi Laura,
+mail -s '(2 Weeks Later) speaker on the simulation lunch meeting' geil.p@unimelb.edu.au <<< 'Hi Paul G,
 
-You are selected to be the speaker (for papers) for the simulation lunch meeting to be held 2 weeks later (07/July/16). (http://smutch.github.io/simslunch_site/index.html)
-Please let me know if you do not want to present a papers:)
+You are selected to be the speaker (for plots) for the simulation lunch meeting to be held 2 weeks later (14/July/16). (http://smutch.github.io/simslunch_site/index.html)
+Please let me know if you do not want to present a plots:)
 
 Cheers,
 Yuxiang'
